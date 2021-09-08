87f80130be086d5dfd7888cf949b2debf1257d11
gstimm <contato.gabrieltimm@gmail.com>
2021-09-08 11:31:35 -0300

    feat: validate auth by server & redirect by client

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,9 @@
 import { useContext, useEffect } from 'react';
 
 import { AuthContext } from '../contexts/AuthContext';
-import { api } from '../services/api';
+import { api } from '../services/apiClient';
+import { setupAPIClient } from '../services/api';
+import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
@@ -19,3 +21,14 @@ export default function Dashboard() {
     </>
   );
 }
+
+export const getServerSideProps = withSSRAuth(async ctx => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+
+  console.log(response.data);
+
+  return {
+    props: {},
+  };
+});
